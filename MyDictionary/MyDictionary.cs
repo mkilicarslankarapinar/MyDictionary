7065ab5d3c70e84400dacc1c09f8288e1c443324
mkilicarslan <mkilicarslankarapinar@gmail.com>
2022-08-31 12:26:28 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary
+    {
+
+        public class MyDictionaries<T>
+        {
+            T[] _array;
+            public MyDictionaries()
+            {
+                _array = new T[0];
+            }
+
+            public void Add(T item)
+            {
+                T[] _tempArray = _array;
+
+                _array = new T[_array.Length + 1];
+                for (int i = 0; i < _tempArray.Length; i++)
+                {
+                    _array[i] = _tempArray[i];
+                }
+                _array[_tempArray.Length] = item;
+            }
+
+            public int Count
+            {
+                get { return _array.Length; }
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static MyDictionary.MyDictionary;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionaries<int> myDictionary = new MyDictionaries<int>();
+            myDictionary.Add(112);
+            myDictionary.Add(112);
+            myDictionary.Add(112);
+            myDictionary.Add(112);
+            myDictionary.Add(112);
+            myDictionary.Add(112);
+            myDictionary.Add(112);
+            Console.WriteLine(myDictionary.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
